a342aab87594265a6379b10bd0c9526f5488c9b8
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-05-27 02:31:00 +0300

    Updated resume portfolio

---
+++ staticfiles/js/main.js
@@ -0,0 +1,13 @@
+let typed = new Typed(".auto-type", {
+  strings: [
+    "makes it happen!",
+    "Ensures Quality Work",
+    "Is a Unique Guy",
+    "Loves Animation",
+    "Loves building things from scratch",
+    "Contact Me and see Changes!",
+  ],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
